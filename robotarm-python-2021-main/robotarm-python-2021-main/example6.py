8aa1a046e5426793667b6d76efc2b65066abc7f9
arsKalishoek <99069312@mydavinci.nl>
2021-09-20 12:49:23 +0200

    oefening 6

---
+++ robotarm-python-2021-main/robotarm-python-2021-main/example6.py
@@ -0,0 +1,59 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 6')
+robotArm.speed = 3
+
+# Jouw python instructies zet je vanaf hier:
+
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+
+
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
